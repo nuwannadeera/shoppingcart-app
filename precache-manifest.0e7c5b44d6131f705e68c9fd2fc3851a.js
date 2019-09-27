322337b9eec2497e9c346233c5c3c8c29754b0a7
nuwannadeera <www.nuwannadeerarocks@gmail.com>
2019-09-27 23:13:46 +0530

    Updates

---
+++ precache-manifest.0e7c5b44d6131f705e68c9fd2fc3851a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af152ebd32160346447a4f06c07c5471",
+    "url": "/shoppingcart-app/index.html"
+  },
+  {
+    "revision": "2ffafeafa74377904759",
+    "url": "/shoppingcart-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6e45f615e356ab9030df",
+    "url": "/shoppingcart-app/static/js/2.f7bfa775.chunk.js"
+  },
+  {
+    "revision": "2ffafeafa74377904759",
+    "url": "/shoppingcart-app/static/js/main.11946968.chunk.js"
+  },
+  {
+    "revision": "dd20377dd6dd01155dc7",
+    "url": "/shoppingcart-app/static/js/runtime-main.4f42cfb3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/shoppingcart-app/static/media/logo.25bf045c.svg"
+  }
+]);
